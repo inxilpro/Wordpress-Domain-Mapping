4a9c1e8aa3429d83c73dc977cae9c0121b9ee677
donncha <donncha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-08-11 19:24:02 +0000

    Use wp_blogs to create the $current_blog object Use $current_blog->site_id to get the correct $current_site
    git-svn-id: http://plugins.svn.wordpress.org/wordpress-mu-domain-mapping/trunk@59253 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sunrise.php
+++ sunrise.php
@@ -11,16 +11,15 @@ $dm_domain = $wpdb->escape( preg_replace( "/^www\./", "", $_SERVER[ 'HTTP_HOST' 
 $domain_mapping_id = $wpdb->get_var( "SELECT blog_id FROM {$wpdb->dmtable} WHERE domain = '{$dm_domain}' LIMIT 1" );
 $wpdb->suppress_errors( false );
 if( $domain_mapping_id ) {
-	$current_blog->blog_id = $domain_mapping_id;
-	$current_blog->site_id = 1;
+	$current_blog = $wpdb->get_row("SELECT * FROM {$wpdb->blogs} WHERE blog_id = '$domain_mapping_id' LIMIT 1");
 	$current_blog->domain = $_SERVER[ 'HTTP_HOST' ];
 	$current_blog->path = '/';
-	$current_blog->public = 1;
 	$blog_id = $domain_mapping_id;
+	$site_id = $current_blog->site_id;
 
 	define( 'COOKIE_DOMAIN', $_SERVER[ 'HTTP_HOST' ] );
 
-	$current_site = $wpdb->get_row( "SELECT * from wp_site LIMIT 0,1" );
+	$current_site = $wpdb->get_row( "SELECT * from {$wpdb->site} WHERE id = '{$current_blog->site_id}' LIMIT 0,1" );
 	define( 'DOMAIN_MAPPING', 1 );
 }
 ?>
