3328fc9e9a1fe4074847942f8298d95bccfc54ab
donncha <donncha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-04 18:13:58 +0000

    Added site_admin page. Added permanent redirection. Added primary domain.
    git-svn-id: http://plugins.svn.wordpress.org/wordpress-mu-domain-mapping/trunk@170452 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- domain_mapping.php
+++ domain_mapping.php
@@ -27,16 +27,20 @@ Author URI: http://ocaoimh.ie/
 
 function dm_add_pages() {
 	add_management_page( 'Domain Mapping', 'Domain Mapping', 'manage_options', 'domainmapping', 'dm_manage_page' );
+	if( is_site_admin() ) {
+		add_submenu_page('wpmu-admin.php', 'Domain Mapping', 'Domain Mapping', 'manage_options', 'dm_admin_page', 'dm_admin_page');
+	}
 }
 add_action( 'admin_menu', 'dm_add_pages' );
 
-function dm_manage_page() {
+function maybe_create_db() {
 	global $wpdb;
-	$wpdb->dmtable = $wpdb->base_prefix . 'domain_mapping';
 
 	if ( VHOST == 'no' ) {
 		die( 'Sorry, domain mapping only works on virtual host installs.' );
 	}
+
+	$wpdb->dmtable = $wpdb->base_prefix . 'domain_mapping';
 	if ( is_site_admin() ) {
 		if($wpdb->get_var("SHOW TABLES LIKE '{$wpdb->dmtable}'") != $wpdb->dmtable) {
 			$wpdb->query( "CREATE TABLE IF NOT EXISTS `{$wpdb->dmtable}` (
@@ -51,68 +55,146 @@ function dm_manage_page() {
 		}
 	}
 
+}
+
+function dm_admin_page() {
+	global $wpdb;
+	maybe_create_db();
+	if ( false == is_site_admin() ) { // paranoid? moi?
+		return false;
+	}
+
+	if ( !empty( $_POST[ 'action' ] ) ) {
+		check_admin_referer( 'domain_mapping' );
+		if ( $_POST[ 'action' ] == 'update' ) {
+			add_site_option( 'dm_ipaddress', $_POST[ 'ipaddress' ] );
+			add_site_option( 'dm_301_redirect', intval( $_POST[ 'permanent_redirect' ] ) );
+		}
+	}
+
+	echo '<h3>' . __( 'Domain Mapping Configuration' ) . '</h3>';
+	echo "<p>" . __( "As a site admin on this site you can set the IP address users need to point their DNS A records at. If you don't know what it is, ping this blog to get the IP address." ) . "</p>";
+	echo "<p>" . __( "If you use round robin DNS or another load balancing technique with more than one IP, enter each address, separating them by commas." ) . "</p>";
+	echo '<form method="POST">';
+	echo '<input type="hidden" name="action" value="update" />';
+	_e( "Server IP Address: " );
+	echo "<input type='text' name='ipaddress' value='" . get_site_option( 'dm_ipaddress' ) . "' /><br />";
+	echo "<input type='checkbox' name='permanent_redirect' value='1' ";
+	echo get_site_option( 'dm_301_redirect' ) == 1 ? "checked='checked'" : "";
+	echo "' /> Permanent redirect.<br />";
+	wp_nonce_field( 'domain_mapping' );
+	echo "<input type='submit' value='Save' />";
+	echo "</form><br />";
+	_e( 'The information you enter here will be shown to your users so they can configure their DNS correctly.' );
+}
+
+function dm_manage_page() {
+	global $wpdb;
+	maybe_create_db();
 
 	if ( !empty( $_POST[ 'action' ] ) ) {
 		$domain = $wpdb->escape( preg_replace( "/^www\./", "", $_POST[ 'domain' ] ) );
+		if ( $domain == '' ) {
+			wp_die( "You must enter a domain" );
+		}
 		check_admin_referer( 'domain_mapping' );
 		switch( $_POST[ 'action' ] ) {
 			case "add":
-				if( null == $wpdb->get_row( "SELECT blog_id FROM {$wpdb->blogs} WHERE domain = '$domain'" ) && null == $wpdb->get_row( "SELECT blog_id FROM {$wpdb->dmtable} WHERE domain = '$domain'" ) )
-					$wpdb->query( "INSERT INTO {$wpdb->dmtable} ( `id` , `blog_id` , `domain` , `active` ) VALUES ( NULL, '" . intval( $wpdb->blogid ) . "', '$domain', '1')" );
+				if( null == $wpdb->get_row( "SELECT blog_id FROM {$wpdb->blogs} WHERE domain = '$domain'" ) && null == $wpdb->get_row( "SELECT blog_id FROM {$wpdb->dmtable} WHERE domain = '$domain'" ) ) {
+					if ( $_POST[ 'primary' ] ) {
+						$wpdb->query( $wpdb->prepare( "UPDATE {$wpdb->dmtable} SET active = 0 WHERE blog_id = %d", $wpdb->blogid ) );
+					}
+					$wpdb->query( $wpdb->prepare( "INSERT INTO {$wpdb->dmtable} ( `id` , `blog_id` , `domain` , `active` ) VALUES ( NULL, %d, %s, %d )", $wpdb->blogid, $domain, $_POST[ 'primary' ] ) );
+				}
 			break;
 			case "delete":
 				$wpdb->query( "DELETE FROM {$wpdb->dmtable} WHERE domain = '$domain'" );
 			break;
-			case "ipaddress":
-				if( is_site_admin() )
-					add_site_option( 'dm_ipaddress', $_POST[ 'ipaddress' ] );
+			case "primary":
+				$wpdb->query( $wpdb->prepare( "UPDATE {$wpdb->dmtable} SET active = 0 WHERE blog_id = %d", $wpdb->blogid ) );
+				if( $domain != 'original' ) {
+					$wpdb->query( $wpdb->prepare( "UPDATE {$wpdb->dmtable} SET active = 1 WHERE domain = %s", $domain ) );
+				}
 			break;
 		}
 	}
 	echo "<div class='wrap'><h2>Domain Mapping</h2>";
 	if ( !file_exists( ABSPATH . '/wp-content/sunrise.php' ) ) {
-		echo "Please copy sunrise.php to " . ABSPATH . "/wp-content/sunrise.php and uncomment the SUNRISE definition in " . ABSPATH . "wp-config.php";
+		if ( is_site_admin() ) {
+			echo "Please copy sunrise.php to " . ABSPATH . "/wp-content/sunrise.php and uncomment the SUNRISE definition in " . ABSPATH . "wp-config.php";
+		} else {
+			echo "This plugin has not been configured correctly yet.";
+		}
 		echo "</div>";
-		die();
+		return false;
 	}
 
 	if ( !defined( 'SUNRISE' ) ) {
-		echo "Please uncomment the line <em>//define( 'SUNRISE', 'on' );</em> in your " . ABSPATH . "wp-config.php";
+		if ( is_site_admin() ) {
+			echo "Please uncomment the line <em>//define( 'SUNRISE', 'on' );</em> in your " . ABSPATH . "wp-config.php";
+		} else {
+			echo "This plugin has not been configured correctly yet.";
+		}
 		echo "</div>";
-		die();
+		return false;
 	}
 
-	if ( is_site_admin() ) {
-		echo '<h3>' . __( 'Site Admin Configuration' ) . '</h3>';
-		echo "<p>" . __( "As a site admin on this site you can set the IP address users need to point their DNS A records at. If you don't know what it is, ping this blog to get the IP address." ) . "</p>";
-		echo "<p>" . __( "If you use round robin DNS or another load balancing technique with more than one IP, enter each address, separating them by commas." ) . "</p>";
-		echo '<form method="POST">';
-		echo '<input type="hidden" name="action" value="ipaddress" />';
-		_e( "Server IP Address: " );
-		echo "<input type='text' name='ipaddress' value='" . get_site_option( 'dm_ipaddress' ) . "' />";
-		wp_nonce_field( 'domain_mapping' );
-		echo "<input type='submit' value='Save' />";
-		echo "</form><br />";
+	if ( false == get_site_option( 'dm_ipaddress' ) ) {
+		if ( is_site_admin() ) {
+			echo "Please set the IP address of your server in the <a href='wpmu-admin.php?page=dm_admin_page'>site admin page</a>.";
+		} else {
+			echo "This plugin has not been configured correctly yet.";
+		}
+		echo "</div>";
+		return false;
 	}
+
 	$domains = $wpdb->get_results( "SELECT * FROM {$wpdb->dmtable} WHERE blog_id = '{$wpdb->blogid}'" );
 	if ( is_array( $domains ) && !empty( $domains ) ) {
-		?><h3><?php _e( 'Active domains on this blog' ); ?></h3><?php
+		?><h3><?php _e( 'Active domains on this blog' ); ?></h3>
+		<table><tr><th>Domain</th><th>Primary</th><th>Delete</th><th>Make Primary</th></tr>
+		<?php
+		$primary_found = 0;
 		foreach( $domains as $details ) {
+			echo "<tr><td>{$details->domain}</td><td style='text-align: center'><strong>";
+			echo $details->active == 0 ? "No" : "Yes";
+			echo "</strong></td><td>";
 			echo '<form method="POST">';
-			echo $details->domain . " ";
 			echo '<input type="hidden" name="action" value="delete" />';
 			echo "<input type='hidden' name='domain' value='{$details->domain}' />";
 			echo "<input type='submit' value='Delete' />";
 			wp_nonce_field( 'domain_mapping' );
-			echo "</form><br />";
+			echo "</form></td><td>";
+			if ( 0 == $primary_found && $details->active == 0 ) {
+				echo '<form method="POST">';
+				echo '<input type="hidden" name="action" value="primary" />';
+				echo "<input type='hidden' name='domain' value='{$details->domain}' />";
+				echo "<input type='submit' value='Make Primary' />";
+				wp_nonce_field( 'domain_mapping' );
+				echo "</form>";
+			}
+			echo "</td></tr>";
+			if ( 0 == $primary_found )
+				$primary_found = $details->active;
 		}
-		?><br /><?php
+		?></table><?php
+		if ( $primary_found == 0 ) {
+			echo "<h3>Primary Domain disabled</h3><p>Your blog will answer on every domain listed, including the original blog url.</p>";
+		} else {
+			echo '<form method="POST">';
+			echo '<input type="hidden" name="action" value="primary" />';
+			echo "<input type='hidden' name='domain' value='original' />";
+			echo "<input type='submit' value='Disable Primary Domain Checking' />";
+			wp_nonce_field( 'domain_mapping' );
+			echo "</form>";
+		}
 	}
 	echo "<h3>" . __( 'Add new domain' ) . "</h3>";
 	echo '<form method="POST">';
 	echo '<input type="hidden" name="action" value="add" />';
-	echo "http://www.<input type='text' name='domain' value='' />/";
+	echo "http://www.<input type='text' name='domain' value='' />/<br />";
 	wp_nonce_field( 'domain_mapping' );
+	echo "<input type='checkbox' name='primary' value='1' /> Primary domain for this blog<br />";
 	echo "<input type='submit' value='Add' />";
 	echo "</form><br />";
 	echo "<p>" . __( 'If your domain name includes a hostname like "blog" or some other prefix before the actual domain name you will need to add a CNAME for that hostname in your DNS pointing at this blog URL. "www" does not count because it will be removed from the domain name.' ) . "</p>";
@@ -134,13 +216,11 @@ function domain_mapping_siteurl( $setting ) {
 	if ( !isset( $return_url[ $wpdb->blogid ] ) ) {
 		$s = $wpdb->suppress_errors();
 
-		// Try matching on the current URL domain and blog first. This will take priorty.
-		$domain = $wpdb->get_var( $wpdb->prepare( "SELECT domain FROM {$wpdb->dmtable} WHERE domain = %s AND blog_id = %d LIMIT 1", preg_replace( "/^www\./", "", $_SERVER[ 'HTTP_HOST' ] ), $wpdb->blogid ) );
-
-		// If no match, then try against the blog ID alone (which we get, without a 'preferred domain' setting,
-		// will be a matter of luck.
-		if ( empty( $domain ) ) {
-			$domain = $wpdb->get_var( $wpdb->prepare( "SELECT domain FROM {$wpdb->dmtable} WHERE blog_id = %d", $wpdb->blogid ) );
+		// get primary domain 
+		$domain = $wpdb->get_var( "SELECT domain FROM {$wpdb->dmtable} WHERE blog_id = '{$wpdb->blogid}' AND active = 1 LIMIT 1" );
+		if ( null == $domain ) {
+			// Try matching on the current URL domain and blog.
+			$domain = $wpdb->get_var( $wpdb->prepare( "SELECT domain FROM {$wpdb->dmtable} WHERE domain = %s AND blog_id = %d LIMIT 1", preg_replace( "/^www\./", "", $_SERVER[ 'HTTP_HOST' ] ), $wpdb->blogid ) );
 		}
 
 		$wpdb->suppress_errors( $s );
@@ -177,11 +257,11 @@ function domain_mapping_post_content( $post_content ) {
 }
 
 // fixes the plugins_url 
-function domain_map_plugins_uri( $full_url, $path=NULL, $plugin=NULL ) {
+function domain_mapping_plugins_uri( $full_url, $path=NULL, $plugin=NULL ) {
 	return get_option( 'siteurl' ) . substr( $full_url, stripos( $full_url, PLUGINDIR ) - 1 );
 }
 
-function domain_map_themes_uri( $full_url ) {
+function domain_mapping_themes_uri( $full_url ) {
 	return get_option( 'siteurl' ) . substr( $full_url, stripos( $full_url, "/wp-content/themes" ) );
 }
 
@@ -194,11 +274,12 @@ if ( defined( 'DOMAIN_MAPPING' ) ) {
 }
 
 function redirect_to_mapped_domain() {
-	global $current_blog;
+	global $current_blog, $wpdb;
 	$protocol = ( 'on' == strtolower($_SERVER['HTTPS']) ) ? 'https://' : 'http://';
 	$url = domain_mapping_siteurl( false );
 	if ( $url && $url != untrailingslashit( $protocol . $current_blog->domain . $current_blog->path ) ) {
-		wp_redirect( $url . $_SERVER[ 'REQUEST_URI' ] );
+		$redirect = get_site_option( 'dm_301_redirect' ) ? '301' : '302';
+		header( "Location: {$url}{$_SERVER[ 'REQUEST_URI' ]}", true, $redirect );
 		exit;
 	}
 }
