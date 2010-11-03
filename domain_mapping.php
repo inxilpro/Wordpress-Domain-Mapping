3a98d9534eaf406a9680728df213a2b64339bbf5
donncha <donncha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-03 11:57:51 +0000

    Added "dm_site_admin()" to support is_site_admin() and is_super_admin()
    git-svn-id: http://plugins.svn.wordpress.org/wordpress-mu-domain-mapping/trunk@307325 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- domain_mapping.php
+++ domain_mapping.php
@@ -50,7 +50,7 @@ function dm_add_pages() {
 	if ( get_site_option( 'dm_user_settings' ) && $current_site->blog_id != $wpdb->blogid && !dm_sunrise_warning( false ) ) {
 		add_management_page( 'Domain Mapping', 'Domain Mapping', 'manage_options', 'domainmapping', 'dm_manage_page' );
 	}
-	if( is_site_admin() ) {
+	if( dm_site_admin() ) {
 		add_submenu_page('wpmu-admin.php', 'Domain Mapping', 'Domain Mapping', 'manage_options', 'dm_admin_page', 'dm_admin_page');
 		add_submenu_page('wpmu-admin.php', 'Domains', 'Domains', 'manage_options', 'dm_domains_admin', 'dm_domains_admin');
 	}
@@ -87,7 +87,7 @@ function maybe_create_db() {
 
 	$wpdb->dmtable = $wpdb->base_prefix . 'domain_mapping';
 	$wpdb->dmtablelogins = $wpdb->base_prefix . 'domain_mapping_logins';
-	if ( is_site_admin() ) {
+	if ( dm_site_admin() ) {
 		$created = 0;
 		if ( $wpdb->get_var("SHOW TABLES LIKE '{$wpdb->dmtable}'") != $wpdb->dmtable ) {
 			$wpdb->query( "CREATE TABLE IF NOT EXISTS `{$wpdb->dmtable}` (
@@ -119,7 +119,7 @@ function maybe_create_db() {
 
 function dm_domains_admin() {
 	global $wpdb, $current_site;
-	if ( false == is_site_admin() ) { // paranoid? moi?
+	if ( false == dm_site_admin() ) { // paranoid? moi?
 		return false;
 	}
 
@@ -242,7 +242,7 @@ function dm_domain_listing( $rows, $heading = '' ) {
 
 function dm_admin_page() {
 	global $wpdb, $current_site;
-	if ( false == is_site_admin() ) { // paranoid? moi?
+	if ( false == dm_site_admin() ) { // paranoid? moi?
 		return false;
 	}
 
@@ -372,7 +372,7 @@ function dm_sunrise_warning( $die = true ) {
 		if ( !$die )
 			return true;
 
-		if ( is_site_admin() ) {
+		if ( dm_site_admin() ) {
 			wp_die( sprintf( __( "Please copy sunrise.php to %s/wp-content/sunrise.php and ensure the SUNRISE definition is in %swp-config.php", 'wordpress-mu-domain-mapping' ), ABSPATH, ABSPATH ) );
 		} else {
 			wp_die( __( "This plugin has not been configured correctly yet.", 'wordpress-mu-domain-mapping' ) );
@@ -381,7 +381,7 @@ function dm_sunrise_warning( $die = true ) {
 		if ( !$die )
 			return true;
 
-		if ( is_site_admin() ) {
+		if ( dm_site_admin() ) {
 			wp_die( sprintf( __( "Please uncomment the line <em>define( 'SUNRISE', 'on' );</em> or add it to your %swp-config.php", 'wordpress-mu-domain-mapping' ), ABSPATH ) );
 		} else {
 			wp_die( __( "This plugin has not been configured correctly yet.", 'wordpress-mu-domain-mapping' ) );
@@ -390,7 +390,7 @@ function dm_sunrise_warning( $die = true ) {
 		if ( !$die )
 			return true;
 
-		if ( is_site_admin() ) {
+		if ( dm_site_admin() ) {
 			wp_die( sprintf( __( "Please edit your %swp-config.php and move the line <em>define( 'SUNRISE', 'on' );</em> above the last require_once() in that file or make sure you updated sunrise.php.", 'wordpress-mu-domain-mapping' ), ABSPATH ) );
 		} else {
 			wp_die( __( "This plugin has not been configured correctly yet.", 'wordpress-mu-domain-mapping' ) );
@@ -412,7 +412,7 @@ function dm_manage_page() {
 	echo "<div class='wrap'><h2>" . __( 'Domain Mapping', 'wordpress-mu-domain-mapping' ) . "</h2>";
 
 	if ( false == get_site_option( 'dm_ipaddress' ) && false == get_site_option( 'dm_cname' ) ) {
-		if ( is_site_admin() ) {
+		if ( dm_site_admin() ) {
 			_e( "Please set the IP address or CNAME of your server in the <a href='wpmu-admin.php?page=dm_admin_page'>site admin page</a>.", 'wordpress-mu-domain-mapping' );
 		} else {
 			_e( "This plugin has not been configured correctly yet.", 'wordpress-mu-domain-mapping' );
@@ -789,5 +789,15 @@ function ra_domain_mapping_field( $column, $blog_id ) {
 	}
 }
 add_action( 'manage_blogs_custom_column', 'ra_domain_mapping_field', 1, 3 );
+
+function dm_site_admin() {
+	if ( function_exists( 'is_super_admin' ) ) {
+		return is_super_admin();
+	} elseif ( function_exists( 'is_site_admin' ) ) {
+		return is_site_admin();
+	} else {
+		return true;
+	}
+}
 
 ?>
